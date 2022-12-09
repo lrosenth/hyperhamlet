9feb6928dd963a4ad804161a516645bfa9fe533a
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2022-12-10 00:51:30 +0100

    ...

--- py-scripts/create_json.py
+++ py-scripts/create_json.py
@@ -1,4 +1,5 @@
 import json
+from pprint import pprint
 from typing import List
 
 from list_node import RawListNode, ListNode
@@ -13,6 +14,7 @@ mariadb_params = {
 }
 
 datamodel = {
+    "$schema": "https://raw.githubusercontent.com/dasch-swiss/dsp-tools/main/knora/dsplib/schemas/ontology.json",
     "project": {
         "shortcode": "0841",
         "shortname": "hyperhamlet",
@@ -20,24 +22,166 @@ datamodel = {
         "descriptions": {
             "en": "Hyperhamlet"
         },
-        "keywords": {
-            "Shakespeare", "cultural history"
-        },
-        "lists": []
+        "keywords": [
+            "Shakespeare",
+            "cultural history"
+        ],
+        "lists": [
+            {
+                "name": "linetypes",
+                "labels": {"en": "Linetypes"},
+                "nodes": [
+                    {
+                        "name": "Text",
+                        "labels": {"en": "Text"}
+                    },
+                    {
+                        "name": "heading",
+                        "labels": {"en": "Heading"}
+                    },
+                    {
+                        "name": "stage",
+                        "labels": {"en": "Stage"}
+                    },
+                    {
+                        "name": "speaker",
+                        "labels": "Speaker"
+                    }
+                ]
+            }
+        ],
+        "ontologies": [{
+            "name": "Hyperhamlet",
+            "description": "Hyperhamlet ontology",
+            "properties": [
+                {
+                    "name": "hasLineId",
+                    "super": ["hasValue"],
+                    "object": "IntValue",
+                    "labels": {"en": "ID"},
+                    "gui_element": "SimpleText",
+                    "gui_attributes": {
+                        "maxlength=": 5,
+                        "size": 6
+                    }
+                },
+                {
+                    "name": "hasLineNumber",
+                    "super": ["hasValue"],
+                    "object": "IntValue",
+                    "labels": {"en": "Line number"},
+                    "gui_element": "SimpleText",
+                    "gui_attributes": {
+                        "maxlength=": 5,
+                        "size": 6
+                    }
+                },
+                {
+                    "name": "hasLinetext",
+                    "super": ["hasValue"],
+                    "object": "TextValue",
+                    "labels": {"en": "Linetext"},
+                    "gui_element": "SimpleText",
+                    "gui_attributes": {
+                        "maxlength=": 255,
+                        "size": 80
+                    }
+                },
+                {
+                    "name": "inAct",
+                    "super": ["hasValue"],
+                    "object": "IntValue",
+                    "labels": {"en": "Act"},
+                    "gui_element": "Spinbox",
+                    "gui_attributes": {
+                        "max": 5.0,
+                        "min": 1.0
+                    }
+                },
+                {
+                    "name": "inScene",
+                    "super": ["hasValue"],
+                    "object": "IntValue",
+                    "labels": {"en": "Scene"},
+                    "gui_element": "Spinbox",
+                    "gui_attributes": {
+                        "max": 7.0,
+                        "min": 1.0
+                    }
+                },
+                {
+                    "name": "hasLineType",
+                    "super": ["hasValue"],
+                    "object": "ListValue",
+                    "labels": {"en": "Linetype"},
+                    "gui_element": "List",
+                    "gui_attributes": {
+                        "hlist": "linetypes"
+                    }
+                }
+            ],
+            "resources": [
+                {
+                    "name": "hamlettext",
+                    "labels": {
+                        "en": "hamlettext"
+                    },
+                    "super": "Resource",
+                    "cardinality": [
+                        {
+                            "propname": "hasLineId",
+                            "gui_order": 1,
+                            "cardinality": "1"
+                        },
+                        {
+                            "propname": "hasLineNumber",
+                            "gui_order": 2,
+                            "cardinality": "1"
+                        },
+                        {
+                            "propname": "hasLinetext",
+                            "gui_order": 3,
+                            "cardinality": "1"
+                        },
+                        {
+                            "propname": "hasLineType",
+                            "gui_order": 4,
+                            "cardinality": "1"
+                        },
+                        {
+                            "propname": "inAct",
+                            "gui_order": 5,
+                            "cardinality": "1"
+                        },
+                        {
+                            "propname": "inScene",
+                            "gui_order": 6,
+                            "cardinality": "1"
+                        },
+                    ]
+                }
+            ]
+        }]
     }
 }
 
 
-def get_list(con: mariadb.Connection, name: str) -> List[RawListNode]:
+def get_list(con: mariadb.Connection, name: str) -> List[ListNode]:
     cursor = con.cursor()
     cursor.execute(f"SELECT * FROM {name}")
     res = cursor.fetchone()
-    nodes: List[RawListNode] = []
+    rawnodes: List[RawListNode] = []
     while res:
-        nodes.append(RawListNode(res))
+        rawnodes.append(RawListNode(res))
         res = cursor.fetchone()
     cursor.close()
-    return nodes
+
+    toprawnodes = [x for x in rawnodes if x.parent is None]
+    toprawnodes.sort(key=lambda node: node.sortorder)
+
+    topnodes = [to_node_with_children(x, rawnodes) for x in toprawnodes]
+
+    return topnodes
 
 
 
@@ -52,16 +196,53 @@ def to_node_with_children(rawnode: RawListNode, rawnodes: List[RawListNode]):
     return node
 
 
+def nodes_to_dict(name: str, comment: str, nodes: List[ListNode]):
+
+    def process_one_node(node: ListNode):
+        tmpobj = {
+            "name": node.name,
+            "labels": {
+                "en": node.name
+            }
+        }
+        if node.children:
+            tmpobj["nodes"] = []
+            for child in node.children:
+                tmpobj["nodes"].append(process_one_node(child))
+        return tmpobj
+
+    listdict = {
+        "name": name,
+        "labels": {
+            "en": name
+        },
+        "comments": {
+            "en": comment
+        },
+    }
+    if nodes:
+        listdict["nodes"] = []
+        for node in nodes:
+            listdict["nodes"].append(process_one_node(node))
+    return listdict
 
 con = mariadb.connect(**mariadb_params)
 
-rawnodes = get_list(con, "quotmarks")
-toprawnodes = [x for x in rawnodes if x.parent is None]
-toprawnodes.sort(key=lambda node: node.sortorder)
+listnames = [
+    "authormarks",
+    "generals",
+    "intertextuals",
+    "languages",
+    "linecategories"
+]
 
-topnodes = [to_node_with_children(x, rawnodes) for x in toprawnodes]
-for y in topnodes:
-    y.printit()
+for listname in listnames:
+    topnodes = get_list(con, listname)
+    listdict = nodes_to_dict(listname, "a comment", topnodes)
+    datamodel["project"]["lists"].append(listdict)
 
+jsonstr = json.dumps(datamodel, indent=3)
+with open("hyperhamlet.json", "w") as outfile:
+    outfile.write(jsonstr)
 
 con.close()
